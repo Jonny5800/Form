2fef164c5604860c76d4b5a6c2ddac1f9cd019b2
Jonny <jonny.hall5800@gmail.com>
2022-02-22 06:39:37 +0000

    To see centre width on mobile

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 document.write("hello JS")
+
+
+$("input:checkbox").click(function() {
+    var bol = $("input:checkbox:checked").length >= 2;
+    $("input:checkbox").not(":checked").attr("disabled", bol);
+});
